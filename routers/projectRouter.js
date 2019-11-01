322e5612708d7c60703dcd8079dee5cdc903d07d
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-01 09:55:37 +0100

    GET, GET by ID, POST: completed

---
+++ routers/projectRouter.js
@@ -0,0 +1,84 @@
+const express = require('express');
+
+const dbProject = require('../data/helpers/projectModel')
+
+const projectRouter = express.Router();
+
+projectRouter.get('/', getAllProjects);
+projectRouter.post('/', validateProject, addProject);
+projectRouter.get('/:id',validateProjectId, getProjectById);
+projectRouter.delete('/:id',validateProjectId, deleteProject);
+
+function getProjectById(req, res) {
+    res.json(req.project);
+}
+
+
+function deleteProject(req, res) {
+
+}
+
+function addProject(req, res) {
+dbProject.insert(req.body)
+.then(project => {
+    res.status(201).json(project)
+})
+.catch(error => {
+    
+    res.status(500).json({
+      message: 'Error adding the new project: ' + error.message,
+    });
+  })
+}
+
+
+function getAllProjects(req, res) {
+dbProject.get()
+.then(projects => {
+    res.status(200).json(projects)
+})
+.catch(error => {
+    res.status(500).json({
+        errorMessage: "projects not available: " + error 
+    })
+})
+}
+
+
+//Middleware
+function validateProjectId(req, res, next) {
+    const id = req.params.id;
+    dbProject.get(id)
+    .then(project => {
+        if(project){
+            req.project = project;
+            next()
+        }else{
+            res.status(404).json({ message: 'Project id does not correspond with an actual Project' });
+        }
+    })
+    .catch(error => {
+        res.status(404).json({ message: "invalid project id: "  + error.message})
+    })
+
+}
+
+function validateProject(req, res, next) {
+    if (Object.keys(req.body).length) {
+        if(req.body){//It still has a bug, fix if you have the time.
+            next();
+
+        }
+        else 
+        {
+            res.status(400).json({ message: "all fields are required!" });
+        }
+        
+      } 
+      else 
+      {
+        res.status(400).json({ message: "missing user data" });
+      }
+}
+
+module.exports = projectRouter
